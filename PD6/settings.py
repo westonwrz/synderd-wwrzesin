74ca0f7ef08d7952c8c9d157d38a96a30867f8aa
Weston Wrzesinski <westonwrzesinski@yahoo.com>
2020-04-06 20:45:36 -0700

    PD7

--- PD6/settings.py
+++ PD6/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'synerd',
+    'backend',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,12 @@ WSGI_APPLICATION = 'PD6.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ubzgyxgl',
+        'HOST' : 'kandula.db.elephantsql.com',
+        'USER' : 'ubzgyxgl',
+        'PASSWORD' : '78a4EG4WM_Ha-qxPmwCDGgPWngCakgWB',
+        'PORT' : '5432'
     }
 }
 
---
+++ backend/models.py
@@ -0,0 +1,104 @@
+from django.db import models
+
+# Create your models here.
+class UserInfo(models.Model):
+    username = models.CharField(primary_key=True, max_length=64)
+    firstname = models.CharField(max_length=64)
+    middlename = models.CharField(max_length=64)
+    lastname = models.CharField(max_length=64)
+    email = models.EmailField()
+    address1 = models.CharField(max_length=128)
+    address2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=64)
+    zipcode = models.IntegerField()
+    employername = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.username
+
+class SubscriptionType(models.Model):
+    subscriptiontypecode = models.IntegerField(primary_key=True)
+    subscriptiontypename = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.subscriptiontypecode
+
+class Service(models.Model):
+    servicecode = models.IntegerField(primary_key=True)
+    servicename = models.CharField(max_length=64)
+    description = models.CharField(max_length=128)
+    premium = models.CharField(max_length=64)
+    allocation = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.servicecode
+
+class Office(models.Model):
+    officecode = models.IntegerField(primary_key=True)
+    officename = models.CharField(max_length=64)
+    attribution = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.officecode
+
+class Organization(models.Model):
+    organizationcode = models.IntegerField(primary_key=True)
+    organizationname = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    datejoined = models.DateField()
+    address1 = models.CharField(max_length=128)
+    address2 = models.CharField(max_length=128)
+    city = models.CharField(max_length=64)
+    state = models.CharField(max_length=64)
+    zipcode = models.IntegerField()
+    phonenumber = models.IntegerField()
+
+    def __str__(self):
+        return self.organizationcode
+
+class Subscriber(models.Model):
+    subscriberID = models.IntegerField(primary_key=True)
+    username = models.ForeignKey(UserInfo, on_delete=models.CASCADE)
+    subscriptiontypecode = models.ForeignKey(SubscriptionType, on_delete=models.CASCADE)
+    servicecode = models.ForeignKey(Service, on_delete=models.CASCADE)
+    requestdate = models.DateField()
+    startdate = models.DateField()
+    enddate = models.DateField()
+    motifofcancellation = models.CharField(max_length=64)
+    beneficiaryID = models.IntegerField()
+
+    def __str__(self):
+        return self.subscriberID
+
+class TransferredSubscription(models.Model):
+    transferID = models.IntegerField(primary_key=True)
+    transferfrom = models.CharField(max_length=64)
+    transferto = models.CharField(max_length=64)
+    requestdate = models.DateField()
+    transferdate = models.DateField()
+    subscriberID = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.transferID
+
+class Officer(models.Model):
+    officecode = models.ForeignKey(Office, on_delete=models.CASCADE)
+    subscriberID = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    startdate = models.DateField()
+    enddate = models.DateField()
+
+    def __str__(self):
+        return self.officecode
+
+class OrganizationMember(models.Model):
+    organizationcode = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    subscriberID = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    startdate = models.DateField()
+    enddate = models.DateField()
+    nativecountry = models.CharField(max_length=64)
+    citizenship = models.CharField(max_length=64)
+    isdelegate = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.organizationcode
